f1e35cdcd9f1eb74945b9b72480b0eb340d70874
Daiwei Liu <dai00021@gmail.com>
2016-04-08 16:08:37 +0800

    creating the NavMesh

---
+++ Assets/RandomMoving.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomMoving : MonoBehaviour {
+
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rb.MovePosition(transform.position+1);
+	}
+}
