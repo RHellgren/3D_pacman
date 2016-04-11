8a6fb7ac9f56b8c0de5903592e42e2579fbedfd3
Daiwei Liu <dai00021@gmail.com>
2016-04-11 13:57:13 +0800

    foundation for AI

--- Assets/RedEnemy.cs
+++ Assets/RedEnemy.cs
@@ -7,9 +7,24 @@ public class RedEnemy : MonoBehaviour {
 	void Start () {
 	
 	}
+
+	public void activate () {
+		//Start to move
+	}
+
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	public void setDestination (Vector3 destination) {
+		
+	}
+
+	public void setMode (int mode) {
+		// 1 attack
+		// 2 Escape
+		// 3 Going Arround
 	}
 }
