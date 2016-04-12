e6503edd89efe6e8649734b3149f6ce188ed7686
Robin Hellgren <rhellgren@icloud.com>
2016-04-12 16:50:28 +0800

    fixed git error

--- Assets/OrangeEnemy.cs
+++ Assets/OrangeEnemy.cs
@@ -66,18 +66,15 @@ public class OrangeEnemy : MonoBehaviour {
 						index = 0;
 				}
 			}
-		}
-<<<<<<< HEAD
-=======
-		if (mode == 3) {
-			destination = idles [index];
-			agent.destination = destination;
-			if (Vector3.Distance (destination, transform.position) < 1.0f) {
-				index++;
-				if (index == 4)
-					index = 0;
+
+			if (mode == 3) {
+				destination = idles [index];
+				agent.destination = destination;
+				if (Vector3.Distance (destination, transform.position) < 1.0f) {
+					index++;
+					if (index == 4)
+						index = 0;
+				}
 			}
 		}
-	}
->>>>>>> 808a6b6272d8725a143a0c0e125da2f8521776a9
 }
